f9feb92e0213fc3926e9ca529c2eee17758bb47e
Йордан Миладинов <yordanm@proton.me>
2024-05-21 00:56:08 +0300

    fix compilation

--- optimization_test.go
+++ optimization_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/ydm/mist"
 )
 
@@ -12,19 +13,31 @@ func TestOptimizeIf(t *testing.T) {
 
 	cases := []string{
 		"(if 1 2 3)",
+		"(if t 2 3)",
+		"(if 0 2 3)",
+		"(if nil 2 3)",
 	}
 
 	want := []string{
 		"6002",
+		"6002",
+		"6003",
+		"6003",
 	}
 
-	have, err := mist.Compile(c, fmt.Sprintf("case%d", i), false, offopt)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for i, c := range cases {
+		have, err := mist.Compile(c, fmt.Sprintf("case%d", i), false, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if have != want {
-		t.Logf("want:\n%s", mist.Decompile(want[i]))
-		t.Logf("have:\n%s", mist.Decompile(have))
+		if diff := cmp.Diff(want[i], have); diff != "" {
+			t.Logf("Case #%d: %s", i, c)
+
+			t.Logf("want:\n%s", mist.Decompile(want[i]))
+			t.Logf("have:\n%s", mist.Decompile(have))
+
+			t.Fatalf(diff)
+		}
 	}
 }
