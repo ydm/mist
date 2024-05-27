c2d820c9ba82dd8e3a41d545e1131a35436b90ae
Yordan Miladinov <yordanm@proton.me>
2024-05-28 01:32:53 +0300

    clean up

--- playground/contracts/Lispiface.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.24;
-
-interface Lispiface {
-    function something() external payable returns (uint256);
-}
--- playground/contracts/Token.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.24;
-
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract Token is ERC20 {
-    constructor() ERC20("Token", "TK") {}
-}
